145a84203481341cfb67c409134bfd606b12968f
JGG52TIC <91055860+JGG52TIC@users.noreply.github.com>
2021-09-20 20:56:25 +0200

    Add files via upload
    Ejercicio Maven

---
+++ nuevoMaven/taller-maven/src/main/java/com/everis/bootcamp/App.java
@@ -0,0 +1,17 @@
+package com.everis.bootcamp;
+
+import java.time.LocalDate;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	LocalDate date = LocalDate.now();
+        System.out.println("Hoy es: " + date);
+    }
+}
